f5658f97dfcc85465fd8ada925a6ea04d687304a
Joe-noh <goflb.jh@gmail.com>
2014-09-14 07:17:29 +0900

    add Declaimer.Config module and config of highlight.js path

--- lib/mix/tasks/declaimer/compile.ex
+++ lib/mix/tasks/declaimer/compile.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.Declaimer.Compile do
+  alias Declaimer.Config
+
   def run(_) do
-    {{html, themes, opts}, _} = Code.eval_file(source_exs)
+    {{html, themes, opts}, _} = Code.eval_file(Config.source_exs)
 
     # generate css !!
     # this does not remove existing css because the user may edit them
@@ -15,7 +17,7 @@ defmodule Mix.Tasks.Declaimer.Compile do
     end
 
     # generate html !!
-    html_file = output_html
+    html_file = Config.output_html
     if File.exists?(html_file), do: File.rm!(html_file)
 
     File.write!(
@@ -24,7 +26,8 @@ defmodule Mix.Tasks.Declaimer.Compile do
         template_eex,
         body: html,
         themes: themes,
-        highlight_theme: highlight_js_theme(opts)
+        highlight_js_theme: highlight_js_theme(opts),
+        highlight_js_path: Config.highlight_js_path
       )
     )
   end
@@ -40,7 +43,7 @@ defmodule Mix.Tasks.Declaimer.Compile do
         <%= Enum.map themes, fn (theme) -> %>
           <link type="text/css" rel="stylesheet" href="css/<%= theme %>.css">
         <% end %>
-        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/<%= highlight_theme %>.min.css">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/styles/<%= highlight_js_theme %>.min.css">
       </head>
       <body>
         <%= body %>
@@ -48,23 +51,14 @@ defmodule Mix.Tasks.Declaimer.Compile do
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/presentation.js"></script>
 
-        <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.2/highlight.min.js"></script>
+        <script type="text/javascript" src="<%= highlight_js_path %>"></script>
         <script type="text/javascript">hljs.initHighlightingOnLoad();</script>
       </body>
     </html>
     """
   end
 
-  defp source_exs do
-    Application.get_env(:declaimer, :source_exs, "presentation.exs")
-  end
-
-  defp output_html do
-    Application.get_env(:declaimer, :output_html, "presentation.html")
-  end
-
   defp highlight_js_theme(opts) do
-    theme = Application.get_env(:declaimer, :highlight_js_theme, "hybrid")
-    Keyword.get(opts, :highlight_js_theme, theme)
+    Keyword.get(opts, :highlight_js_theme, Config.highlight_js_theme)
   end
 end
